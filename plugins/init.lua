7728f9bed2cdefa7053fb06da1e2aca72f88ae72
Ataxia123 <vp.ca94@gmail.com>
2023-07-04 10:37:53 -0500

    copilot

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,9 @@
 return {
   -- You can disable default plugins as follows:
-  { "goolord/alpha-nvim",           enabled = false },
+  { "goolord/alpha-nvim", enabled = true },
   { "max397574/better-escape.nvim", enabled = false },
   -- You can also add new plugins here as well:
-      {
+  {
     "zbirenbaum/copilot.lua",
     -- Lazy load when event occurs. Events are triggered
     -- as mentioned in:
@@ -12,36 +12,42 @@ return {
     -- You can also have it load at immediately at
     -- startup by commenting above and uncommenting below:
     lazy = false,
-         config = function()
-       require("copilot").setup({})
-     end,
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+          auto_trigger = true,
+        },
+      }
+    end,
   },
 
-  {'ThePrimeagen/harpoon',
-        keys = {
-            { '<leader>ha', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', desc = "Toggle harpoon menu" },
-            { '<leader>hf', '<cmd>lua require("harpoon.mark").toggle_file() <cr>',    desc = "Add file to harpoon list" },
-            { '<leader>hh', '<cmd>lua require("harpoon.ui").nav_next() <cr>',    desc = "Nav Next" },
-            { '<leader>hg', '<cmd>lua require("harpoon.ui").nav_prev() <cr>',    desc = "Nav Prev" },
-        },
-
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-            }
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      { "<leader>ha", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', desc = "Toggle harpoon menu" },
+      { "<leader>hf", '<cmd>lua require("harpoon.mark").toggle_file() <cr>', desc = "Add file to harpoon list" },
+      { "<leader>hh", '<cmd>lua require("harpoon.ui").nav_next() <cr>', desc = "Nav Next" },
+      { "<leader>hg", '<cmd>lua require("harpoon.ui").nav_prev() <cr>', desc = "Nav Prev" },
     },
 
-  { "anuvyklack/windows.nvim",
-   requires = {
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+
+  {
+    "anuvyklack/windows.nvim",
+    requires = {
       "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim"
-   },
-   config = function()
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
       vim.o.winwidth = 10
       vim.o.winminwidth = 10
       vim.o.equalalways = false
-      require('windows').setup()
-   end
-},
+      require("windows").setup()
+    end,
+  },
 
   { "lvimuser/lsp-inlayhints.nvim", config = true },
   {
@@ -51,14 +57,14 @@ return {
       require("neoscroll").setup {
         -- All these keys will be mapped to their corresponding default scrolling animation
         mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil,       -- Default easing function
-        pre_hook = nil,              -- Function to run before the scrolling animation starts
-        post_hook = nil,             -- Function to run after the scrolling animation ends
-        performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+        performance_mode = false, -- Disable "Performance Mode" on all buffers.
       }
     end,
   },
@@ -73,7 +79,7 @@ return {
         -- * a percentage of the width / height of the editor when <= 1
         -- * a function that returns the width or the height
         width = 120, -- width of the Zen window
-        height = 1,  -- height of the Zen window
+        height = 1, -- height of the Zen window
         -- by default, no options are changed for the Zen window
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
@@ -91,12 +97,12 @@ return {
         -- comment the lines to not apply the options
         options = {
           enabled = true,
-          ruler = false,                -- disables the ruler text in the cmd line area
-          showcmd = false,              -- disables the command in the last line of the screen
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
         },
-        twilight = { enabled = true },  -- enable to start Twilight when zen mode opens
+        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
         gitsigns = { enabled = false }, -- disables git signs
-        tmux = { enabled = false },     -- disables the tmux statusline
+        tmux = { enabled = false }, -- disables the tmux statusline
         -- this will change the font size on kitty when in zen mode
         -- to make this work, you need to set the following kitty options:
         -- - allow_remote_control socket-only
